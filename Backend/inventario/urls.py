1248f9f6f4ec465753184c458078b1d6bfd9b363
juanm4 <juanm4morales@gmail.com>
2023-09-03 22:25:35 -0300

    Cambio uris api rest (nombres plurales y separados por '-')

--- Backend/inventario/urls.py
+++ Backend/inventario/urls.py
@@ -3,13 +3,13 @@ from rest_framework import routers
 from inventario import views
 
 router = routers.DefaultRouter()
-router.register(r'tipoinsumo', views.TipoInsumoCRUD, 'tipoinsumo')
-router.register(r'insumo', views.InsumoCRUD, 'insumo')
-router.register(r'tipoherramienta', views.TipoHerramientaCRUD, 'tipoherramienta')
-router.register(r'herramienta', views.HerramientaCRUD, 'herramienta')
-router.register(r'ordenretiro', views.OrdenRetiroCRUD, 'ordenretiro')
-router.register(r'ajustestock', views.AjusteStockCRUD, 'ajustestock')
-router.register(r'estadoherramienta', views.EstadoHerramientaCRUD, 'estadoherramienta')
+router.register(r'tipos-insumo', views.TipoInsumoCRUD, 'tipos-insumo')
+router.register(r'insumos', views.InsumoCRUD, 'insumos')
+router.register(r'tipos-herramienta', views.TipoHerramientaCRUD, 'tipos-herramienta')
+router.register(r'herramientas', views.HerramientaCRUD, 'herramientas')
+router.register(r'ordenes-retiro', views.OrdenRetiroCRUD, 'ordenes-retiro')
+router.register(r'ajustes-stock', views.AjusteStockCRUD, 'ajustes-stock')
+router.register(r'estado-herramientas', views.EstadoHerramientaCRUD, 'estado-herramientas')
 
 urlpatterns = [
     path('', include(router.urls)),
