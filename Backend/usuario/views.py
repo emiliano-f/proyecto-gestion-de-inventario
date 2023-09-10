7ce7bfe9da4e761c3121683441e60b6da021a3c8
emiliano-f <def.emiliano@gmail.com>
2023-09-09 22:54:14 -0300

    auth.urls added

--- Backend/usuario/views.py
+++ Backend/usuario/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from . import serializer
 from . import models
 
 class CustomModelViewSet(viewsets.ModelViewSet):
-    http_method_names = ['post', 'get', 'put']
+    http_method_names = ['post', 'get', 'put', 'delete']
 
 class UsuarioCRUD(CustomModelViewSet):
     serializer_class = serializer.UsuarioSerializer
