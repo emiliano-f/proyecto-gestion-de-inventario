4475ab1d62381a389634b59f651fa7e86cb1ab63
emiliano-f <def.emiliano@gmail.com>
2023-09-29 23:59:19 -0300

    OrdenServicio view: returns apellido, nombre and id from Usuario

--- Backend/tarea/models.py
+++ Backend/tarea/models.py
@@ -33,7 +33,7 @@ class OrdenServicio(models.Model):
 
     id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey("usuario.Usuario", verbose_name=("Id del usuario"), on_delete=models.DO_NOTHING)
-    tarea = models.ForeignKey("tarea.Tarea", verbose_name=(""), on_delete=models.DO_NOTHING)
+    tarea = models.ForeignKey("tarea.Tarea", verbose_name=(""), on_delete=models.DO_NOTHING, null=True)
     fechaGeneracion = models.DateField(auto_now=False, auto_now_add=False)
     sector = models.CharField(max_length=255, null=True)
     descripcion = models.CharField(max_length=255, null=True)
