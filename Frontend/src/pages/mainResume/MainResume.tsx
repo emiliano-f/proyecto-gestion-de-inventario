a11a41d4789b513503103d1409b3261631785f85
Mrtc101 <martincogo4@gmail.com>
2023-10-01 18:41:03 -0300

    recuperando home

--- Frontend/src/pages/mainResume/MainResume.tsx
+++ Frontend/src/pages/mainResume/MainResume.tsx
@@ -1,4 +1,36 @@
+
 import "./mainResume.scss"
+
+import TopBox from "../../components/chartComponents/topBox/TopBox"
+import ChartBox from "../../components/chartComponents/chartBox/ChartBox"
+import BarChartBox from "../../components/chartComponents/barChartBox/BarChartBox"
+import { PieChartBox } from "../../components/chartComponents/pieChartBox/PieChartBox"
+import { BigChartBox } from "../../components/chartComponents/bigChartBox/BigChartBox"
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
+
+function MainResume() {
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2"><ChartBox {...chartBoxUser} /></div>
+      <div className="box box3"><ChartBox {...chartBoxProduct} /></div>
+      <div className="box box4"><PieChartBox /></div>
+      <div className="box box5"><ChartBox {...chartBoxConversion} /></div>
+      <div className="box box6"><ChartBox {...chartBoxRevenue} /></div>
+      <div className="box box7"><BigChartBox /></div>
+      <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
+      <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
+    </div>
+  )
+}
+
+export default MainResume;
+
+
+/*
+export default Home
 
 import RepositionChart from "../../components/chartComponents/repostionChart/RepostionChart"
 import { inventarioReposition,herramientaReposition } from "../../data/resumeData"
@@ -34,22 +66,4 @@ function MainReusme() {
     </div>
   )
 }
-export default MainReusme
-
-/*
-  import TopBox from "../../components/chartComponents/topBox/TopBox"
-  import ChartBox from "../../components/chartComponents/chartBox/ChartBox"
-  import BarChartBox from "../../components/chartComponents/barChartBox/BarChartBox"
-  import { PieChartBox } from "../../components/chartComponents/pieChartBox/PieChartBox"
-  import { BigChartBox } from "../../components/chartComponents/bigChartBox/BigChartBox"
-  import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
-  <div className="box box1"><TopBox /></div>
-  <div className="box box2"><ChartBox {...chartBoxUser} /></div>
-  <div className="box box3"><ChartBox {...chartBoxProduct} /></div>
-  <div className="box box4"><PieChartBox /></div>
-  <div className="box box5"><ChartBox {...chartBoxConversion} /></div>
-  <div className="box box6"><ChartBox {...chartBoxRevenue} /></div>
-  <div className="box box7"><BigChartBox /></div>
-  <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
-  <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
 */
