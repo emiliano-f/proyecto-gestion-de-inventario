c8e57a1df7de59c80fd7266fb4f84dd3f04879d8
emiliano-f <def.emiliano@gmail.com>
2023-10-11 18:31:01 -0300

    InsumoCRUD: minimum refactorization

--- Backend/inventario/views.py
+++ Backend/inventario/views.py
@@ -30,29 +30,36 @@ class InsumoCRUD(viewsets.ViewSet):
 
     def create(self, request):
         serializer_class = serializer.InsumoSerializer(data=request.data)
-        if serializer_class.is_valid():
-            serializer_class.save()
-            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
-        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer_class.is_valid(raise_exception=True):
+        serializer_class.save()
+        return Response(serializer_class.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk):
         try:
             insumo = models.Insumo.objects.get(id=pk)
         except: 
             return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer_class = serializer.InsumoTipoInsumoSerializer(insumo)
         return Response(serializer_class.data)
 
     def update(self, request, pk):
-        insumo = models.Insumo.objects.get(id=pk)
+        try:
+            insumo = models.Insumo.objects.get(id=pk)
+        except: 
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer_class = serializer.InsumoSerializer(insumo, data=request.data)
-        if serializer_class.is_valid():
-            serializer_class.save()
-            return Response(serializer_class.data)
-        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer_class.is_valid(raise_exception=True):
+        serializer_class.save()
+        return Response(serializer_class.data)
 
     def destroy(self, request, pk):
-        insumo = models.Insumo.objects.get(id=pk)
+        try:
+            insumo = models.Insumo.objects.get(id=pk)
+        except: 
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         insumo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
