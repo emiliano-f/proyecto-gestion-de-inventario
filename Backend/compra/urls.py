102ef0efeb7e935c8d2be05528db00fdd62dcccc
emiliano-f <def.emiliano@gmail.com>
2023-10-12 01:38:55 -0300

    Added urls for PedidoInsumo and DetallePedido. Added methods for DetallePedidoCRUD

--- Backend/compra/urls.py
+++ Backend/compra/urls.py
@@ -9,4 +9,8 @@ router.register(r'detalle-pedidos', views.DetallePedidoCRUD, 'detalle-pedidos')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('detalle-pedidos/', views.DetallePedidoCRUD.as_view({'get':'list', 'post':'create'}), name='detalle-pedidos'),
+    path('detalle-pedidos/<int:pk>/', views.DetallePedidoCRUD.as_view({'get':'retrieve','put':'update','delete':'destroy'}), name='detalle-pedidos-id'),
+    path('pedidos-insumo/', views.PedidoInsumoCRUD.as_view({'get':'list', 'post':'create'}), name='pedidos-insumo'),
+    path('pedidos-insumo/<int:pk>/', views.PedidoInsumoCRUD.as_view({'get':'retrieve','put':'update','delete':'destroy'}), name='pedidos-insumo-id'),
 ]
