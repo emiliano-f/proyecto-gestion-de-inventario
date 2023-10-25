38fa2d6e41832d67b7b761e2dfba0e9f6542e678
emiliano-f <def.emiliano@gmail.com>
2023-10-25 18:20:56 -0300

    Added url for UsuarioCRUD

--- Backend/settings/urls.py
+++ Backend/settings/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import auxs_fn
 from rest_framework.documentation import include_docs_urls
+from . import auxs_fn
 
 urlpatterns = [
     path('admin/', admin.site.urls),
