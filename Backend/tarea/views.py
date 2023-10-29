3c285fc727276f359a860668aa9b6414fd878df6
emiliano-f <def.emiliano@gmail.com>
2023-10-28 22:10:33 -0300

    Minor fix

--- Backend/tarea/views.py
+++ Backend/tarea/views.py
@@ -248,7 +248,7 @@ class SectorListCRUD(LoginRequiredMixin, viewsets.ViewSet):
 
     def retrieve(self, request, nombre):
         try:
-            sectores = models.Sector.objects.filter(nombre=nombre).all()
+            sectores = models.Sector.objects.filter(edificio=nombre).all()
         except: 
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer_class = serializer.SectorSubsectorSerializer(sectores, many=True)
