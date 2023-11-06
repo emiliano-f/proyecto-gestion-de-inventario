cd12c537e95bb4241385f487e572356865ff1547
Mrtc101 <martincogo4@gmail.com>
2023-11-05 23:06:03 -0300

    Redirección al Login del Front ante un Forbiden

--- Frontend/src/pages/login/Login.tsx
+++ Frontend/src/pages/login/Login.tsx
@@ -7,19 +7,7 @@ import { Link } from 'react-router-dom';
 import { getToken, Login as login } from "../../Api/apiService"
 
 function Login() {
-  // /admin/login
-
-  const [haveToken,setHaveToken] = useState(false);
-
-  if(!haveToken){
-      getToken()
-      .then((r)=>{
-          console.log(r.headers['x-csrftoken'])
-          setHaveToken(true);
-      })
-      .catch((e)=>console.log(e,"no se obtuvo el token"))
-  }
-  
+  // /admin/login 
   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -33,7 +21,7 @@ function Login() {
       console.log(formData);
       login(formData)
       .then((r)=>{console.log(r),setValidated(true);})
-      .catch((e)=>(console.log(e)))
+      .catch((e)=>(console.log("")))
     }
   };
   
@@ -49,7 +37,7 @@ function Login() {
             </div>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Usuario</Form.Label>
-              <Form.Control name="user" type="string" placeholder="Ingrese nombre de usuario" />
+              <Form.Control name="username" type="string" placeholder="Ingrese nombre de usuario" />
               {/*<Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
